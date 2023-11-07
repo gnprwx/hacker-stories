a2bb418b2e77875c11cf85bc7c93d1ac6537c8b6
Greg Pappas <gnpappas@proton.me>
2023-11-06 18:27:46 -1000

    Add test array

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import "./App.css";
 function getTitle(title: string): string {
     return title;
 }
+const numArr: number[] = [1, 2, 3, 4, 5, 6];
 const App = () => {
     return (
         <>
             <h1>{getTitle("Gregory")}</h1>
             <label htmlFor="name">Name: </label>
             <input type="text" id="name" />
+            <h3>
+                {numArr.map((num) => {
+                    return num;
+                })}
+            </h3>
         </>
     );
 };

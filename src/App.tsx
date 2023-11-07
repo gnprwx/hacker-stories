6a37b9202abdd8e5088089f73f6999ee04c3887a
Greg Pappas <gnpappas@proton.me>
2023-11-06 20:08:59 -1000

    Minor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -18,51 +18,44 @@ const list = [
         objectID: 1,
     },
 ];
-const App = () => {
-    return (
-        <>
-            <div className="center">
-                <h1>Hacker Stories</h1>
-                <Search />
-            </div>
-            <hr />
-            <List />
-        </>
-    );
-};
+const App = () => (
+    <>
+        <div className="center">
+            <h1>Hacker Stories</h1>
+            <Search />
+        </div>
+        <hr />
+        <List />
+    </>
+);
+const List = () => (
+    <ul>
+        {list.length !== 0 ? (
+            list.map((post) => {
+                return (
+                    <li key={post.objectID}>
+                        <span>
+                            <a href={post.url} target="_blank">
+                                {post.title}
+                            </a>
+                        </span>
+                        <span> - {post.author}</span>
+                        <span> [comments: {post.num_comments}]</span>
+                        <span> [points: {post.points}]</span>
+                    </li>
+                );
+            })
+        ) : (
+            <h1>Oops.</h1>
+        )}
+    </ul>
+);
 
-const List = () => {
-    return (
-        <ul>
-            {list.length !== 0 ? (
-                list.map((post) => {
-                    return (
-                        <li key={post.objectID}>
-                            <span>
-                                <a href={post.url} target="_blank">
-                                    {post.title}
-                                </a>
-                            </span>
-                            <span> - {post.author}</span>
-                            <span> [comments: {post.num_comments}]</span>
-                            <span> [points: {post.points}]</span>
-                        </li>
-                    );
-                })
-            ) : (
-                <h1>Oops.</h1>
-            )}
-        </ul>
-    );
-};
-
-const Search = () => {
-    return (
-        <>
-            <label htmlFor="search">Search: </label>
-            <input type="text" id="search" />
-        </>
-    );
-};
+const Search = () => (
+    <>
+        <label htmlFor="search">Search: </label>
+        <input type="text" id="search" />
+    </>
+);
 
 export default App;

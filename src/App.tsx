8895cf3920fce22eee88d44106612972e503a39a
Greg Pappas <gnpappas@proton.me>
2023-11-07 19:55:59 -1000

    Add paragraph to show search parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 
-const list = [
-    {
-        title: "React",
-        url: "https://reactjs.org/",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: "Redux",
-        url: "https://redux.js.org/",
-        author: "Dan Abramov, Andrew Clark",
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
 const App = () => {
+    const stories = [
+        {
+            title: "React",
+            url: "https://reactjs.org/",
+            author: "Jordan Walke",
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: "Redux",
+            url: "https://redux.js.org/",
+            author: "Dan Abramov, Andrew Clark",
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
     return (
         <>
             <div className="center">
@@ -27,38 +27,37 @@ const App = () => {
                 <Search />
             </div>
             <hr />
-            <List />
+            <Posts list={stories} />
         </>
     );
 };
-const List = () => {
-    return (
-        <ul>
-            {list.length !== 0 ? (
-                list.map((post) => {
-                    return (
-                        <li key={post.objectID}>
-                            <span>
-                                <a href={post.url} target="_blank">
-                                    {post.title}
-                                </a>
-                            </span>
-                            <span> - {post.author}</span>
-                            <span> [comments: {post.num_comments}]</span>
-                            <span> [points: {post.points}]</span>
-                        </li>
-                    );
-                })
-            ) : (
-                <h1>There's nothing to see here.</h1>
-            )}
-        </ul>
-    );
-};
+
+const Posts = (props) => (
+    <ul>
+        {props.list.map((post) => (
+            <Post key={post.objectID} post={post} />
+        )
+        )}
+    </ul>
+);
+
+const Post = ({ post }) => (
+    <li>
+        <span>
+            <a href={post.url} target="_blank">
+                {post.title}
+            </a>
+        </span>
+        <span> - {post.author}</span>
+        <span> [comments: {post.num_comments}]</span>
+        <span> [points: {post.points}]</span>
+    </li>
+)
 
 const Search = () => {
+    const [search, setSearch] = useState('');
     const handleChange = (e): void => {
-        console.log(e.target.value);
+        setSearch(e.target.value);
     }
     const handleBlur = (e): void => {
         console.log('lost focus');
@@ -68,6 +67,7 @@ const Search = () => {
         <>
             <label htmlFor="search">Search: </label>
             <input type="text" id="search" onBlur={handleBlur} onChange={handleChange} />
+            <p>Searching for: {search}</p>
         </>
     );
 };
